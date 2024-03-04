799993ab7640aa528ea4dd1b5744c2b9925635ec
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 02:23:35 -0500

    added funcionality for list colors

--- src/main/java/com/example/socialnetwork/controllers/catalogs/ListColorController.java
+++ src/main/java/com/example/socialnetwork/controllers/catalogs/ListColorController.java
@@ -1,2 +1,28 @@
-package com.example.socialnetwork.controllers.catalogs;public class ListColorController {
+package com.example.socialnetwork.controllers.catalogs;
+
+import com.example.socialnetwork.entities.Color;
+import com.example.socialnetwork.services.catalogs.ListColorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Catalogs", description = "Catalogs management operations")
+@RestController
+public class ListColorController {
+
+    @Autowired
+    private ListColorService listColorService;
+
+    @Operation(summary = "List colors", description = "List all colors", tags = { "Catalogs" })
+    @GetMapping("/catalogs/colors/")
+    public ResponseEntity listColors(Pageable pageable) {
+        Page<Color> colors = listColorService.listColors(pageable);
+        return ResponseEntity.ok(colors);
+    }
+
 }
--- src/main/java/com/example/socialnetwork/services/catalogs/ListColorService.java
+++ src/main/java/com/example/socialnetwork/services/catalogs/ListColorService.java
@@ -1,2 +1,21 @@
-package com.example.socialnetwork.services.catalogs;public class ListColorService {
+package com.example.socialnetwork.services.catalogs;
+
+import com.example.socialnetwork.entities.Color;
+import com.example.socialnetwork.repositories.catalogs.ColorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ListColorService {
+
+    @Autowired
+    private ColorRepository colorRepository;
+
+    public Page<Color> listColors(Pageable pageable) {
+        return colorRepository.findAll(pageable);
+    }
 }
