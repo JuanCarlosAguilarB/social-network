68a875a82daa76c65b54aef2da384dc10aa0adaf
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 01:11:00 -0500

    create services for get user by user name

--- src/main/java/com/example/socialnetwork/services/user/GetUserByUsername.java
+++ src/main/java/com/example/socialnetwork/services/user/GetUserByUsername.java
@@ -1,2 +1,22 @@
-package com.example.socialnetwork.services.user;public class GetUserByUsername {
+package com.example.socialnetwork.services.user;
+
+import com.example.socialnetwork.entities.User;
+import com.example.socialnetwork.exceptions.UserNotFoundException;
+import com.example.socialnetwork.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetUserByUsername {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return user;
+    }
 }
