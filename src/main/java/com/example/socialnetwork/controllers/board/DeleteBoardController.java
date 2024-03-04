51491b9e24871aa1267ba19de63610357891c41e
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 02:23:24 -0500

    added funcionality for delete a board

--- src/main/java/com/example/socialnetwork/controllers/board/DeleteBoardController.java
+++ src/main/java/com/example/socialnetwork/controllers/board/DeleteBoardController.java
@@ -1,2 +1,30 @@
-package com.example.socialnetwork.controllers.board;public class DeleteBoard {
+package com.example.socialnetwork.controllers.board;
+
+
+import com.example.socialnetwork.services.board.DeleteBoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Tag(name = "Board", description = "Board management operations")
+@RestController
+public class DeleteBoardController {
+
+    @Autowired
+    private DeleteBoardService deleteBoardServices;
+
+    @Operation(summary = "Delete a board", description = "Deletes a board by its id", tags = { "Board" })
+    @DeleteMapping("/board/{boardId}/")
+    public ResponseEntity deleteBoard(@PathVariable UUID boardId) {
+        deleteBoardServices.deleteBoard(boardId);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/example/socialnetwork/services/board/DeleteBoardService.java
+++ src/main/java/com/example/socialnetwork/services/board/DeleteBoardService.java
@@ -1,2 +1,25 @@
-package com.example.socialnetwork.services.board;public class DeleteBoardService {
+package com.example.socialnetwork.services.board;
+
+import com.example.socialnetwork.exceptions.BoardNotFound;
+import com.example.socialnetwork.repositories.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class DeleteBoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void deleteBoard(UUID boardId) {
+
+        if (!boardRepository.existsById(boardId)) {
+            throw new BoardNotFound("Error deleting board: Board not found");
+        }
+
+        boardRepository.deleteById(boardId);
+
+    }
 }
