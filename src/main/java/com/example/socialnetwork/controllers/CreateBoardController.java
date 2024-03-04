750e363d913867ab2649ce82c519740a73ed17ca
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 02:24:06 -0500

    changes in board services, now we can create a topic

--- src/main/java/com/example/socialnetwork/controllers/CreateBoardController.java
+++ src/main/java/com/example/socialnetwork/controllers/CreateBoardController.java
@@ -3,9 +3,12 @@ package com.example.socialnetwork.controllers;
 import com.example.socialnetwork.entities.Board;
 import com.example.socialnetwork.services.CreateBoardServices;
 import com.example.socialnetwork.services.user.GetUserByUsername;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Tag(name = "Board", description = "Board management operations")
 @RestController
 public class CreateBoardController {
 
@@ -24,6 +28,7 @@ public class CreateBoardController {
     @Autowired
     private GetUserByUsername getUserByUsername;
 
+    @Operation(summary = "Create a new board", description = "Creates a new board for the authenticated user", tags = { "Board" })
     @PostMapping("/board/")
     public ResponseEntity createBoard(@RequestBody CreateBoardRequest createBoardRequest, Authentication auth) {
 
@@ -40,7 +45,7 @@ public class CreateBoardController {
         createBoardServices.createBoard(
                 board
         );
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
--- src/main/java/com/example/socialnetwork/services/board/CreateTopicService.java
+++ src/main/java/com/example/socialnetwork/services/board/CreateTopicService.java
@@ -1,2 +1,19 @@
-package com.example.socialnetwork.services.board;public class TopicServise {
+package com.example.socialnetwork.services.board;
+
+import com.example.socialnetwork.entities.Topic;
+import com.example.socialnetwork.repositories.board.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateTopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public void createTopic(Topic topic) {
+
+        topicRepository.save(topic);
+
+    }
 }
