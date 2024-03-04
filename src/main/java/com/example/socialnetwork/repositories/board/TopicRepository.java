aae562521f247b768b7e3a2f50c97fa298addfbf
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 02:22:00 -0500

    create repository for color and topic

--- src/main/java/com/example/socialnetwork/repositories/board/TopicRepository.java
+++ src/main/java/com/example/socialnetwork/repositories/board/TopicRepository.java
@@ -1,2 +1,9 @@
-package com.example.socialnetwork.repositories.board;public class TopicRepository {
+package com.example.socialnetwork.repositories.board;
+
+import com.example.socialnetwork.entities.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TopicRepository extends JpaRepository<Topic, UUID> {
 }
--- src/main/java/com/example/socialnetwork/repositories/catalogs/ColorRepository.java
+++ src/main/java/com/example/socialnetwork/repositories/catalogs/ColorRepository.java
@@ -1,2 +1,7 @@
-package com.example.socialnetwork.repositories.catalogs;public class ColorRepository {
+package com.example.socialnetwork.repositories.catalogs;
+
+import com.example.socialnetwork.entities.Color;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColorRepository extends JpaRepository<Color, Integer> {
 }
