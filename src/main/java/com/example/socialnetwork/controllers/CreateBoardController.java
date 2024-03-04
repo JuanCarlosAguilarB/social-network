a9a38ec58f678fd340df09d22218152aea35141c
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2024-03-04 01:11:51 -0500

    changes in controlser create board, now, to storage the user than to create to board

--- src/main/java/com/example/socialnetwork/controllers/CreateBoardController.java
+++ src/main/java/com/example/socialnetwork/controllers/CreateBoardController.java
@@ -2,13 +2,17 @@ package com.example.socialnetwork.controllers;
 
 import com.example.socialnetwork.entities.Board;
 import com.example.socialnetwork.services.CreateBoardServices;
+import com.example.socialnetwork.services.user.GetUserByUsername;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class CreateBoardController {
@@ -17,13 +21,20 @@ public class CreateBoardController {
     @Autowired
     private CreateBoardServices createBoardServices;
 
+    @Autowired
+    private GetUserByUsername getUserByUsername;
 
     @PostMapping("/board/")
-    public ResponseEntity createBoard(@RequestBody CreateBoardRequest createBoardRequest) {
+    public ResponseEntity createBoard(@RequestBody CreateBoardRequest createBoardRequest, Authentication auth) {
+
+        // Extract userId of token
+        String username = auth.getName();
+        UUID userId = getUserByUsername.getUserByUsername(username).getId();
 
         Board board = Board.builder()
                 .name(createBoardRequest.getName())
                 .description(createBoardRequest.getDescription())
+                .userId(userId)
                 .build();
 
         createBoardServices.createBoard(
